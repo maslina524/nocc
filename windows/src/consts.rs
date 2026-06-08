890e854f75d11c5caf28cf7f9b2b1dea4a77ee92
maslina524 <luk.kirill2804@gmail.com>
2026-06-08 13:12:06 +0300

    Added const NULL to consts.rs

--- windows/src/consts.rs
+++ windows/src/consts.rs
@@ -1,5 +1,10 @@
+use core::ptr;
+use core::ffi::c_void;
+
 use crate::types::*;
 
 pub const STD_INPUT_HANDLE: STD_HANDLE = 4294967286u32;
 pub const STD_OUTPUT_HANDLE: STD_HANDLE = 4294967285u32;
 pub const STD_ERROR_HANDLE: STD_HANDLE = 4294967284u32;
+
+pub const NULL: *const c_void = ptr::null();
