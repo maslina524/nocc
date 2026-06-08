b4eba5abfcd631c4d2e73401313346296abaaa2b
maslina524 <luk.kirill2804@gmail.com>
2026-06-08 13:15:04 +0300

    Added method print to Io

--- src/io.rs
+++ src/io.rs
@@ -1,6 +1,6 @@
 use windows::types::HANDLE;
-use windows::consts::{STD_INPUT_HANDLE, STD_OUTPUT_HANDLE, STD_ERROR_HANDLE};
-use windows::link::GetStdHandle;
+use windows::consts::{STD_INPUT_HANDLE, STD_OUTPUT_HANDLE, STD_ERROR_HANDLE, NULL};
+use windows::link::{GetStdHandle, WriteConsoleW};
 
 pub struct Io {
     stdout_handle: HANDLE,
@@ -15,5 +15,15 @@ impl Io {
         let input_handle = unsafe { GetStdHandle(STD_INPUT_HANDLE) };
 
         Io { stdout_handle, stderr_handle, input_handle }
+    }
+
+    pub fn print(&self, string: &str) {
+        let mut buf = [0; 1024];
+        let written_to_utf16 = windows::utils::utf8_to_utf16le(string, &mut buf) as u32;
+        let mut written = 0u32;
+
+        unsafe { 
+            WriteConsoleW(self.stdout_handle, buf.as_ptr() as *const u16, written_to_utf16, &mut written, NULL);
+        }
     }
 }
