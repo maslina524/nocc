294884e12834956a56049470f0272e66d0bf3800
maslina524 <luk.kirill2804@gmail.com>
2026-06-08 12:37:13 +0300

    Added method new to Io

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,19 @@
 use windows::types::HANDLE;
+use windows::consts::{STD_INPUT_HANDLE, STD_OUTPUT_HANDLE, STD_ERROR_HANDLE};
+use windows::link::GetStdHandle;
 
 pub struct Io {
-    std_handle: HANDLE
+    stdout_handle: HANDLE,
+    stderr_handle: HANDLE,
+    input_handle: HANDLE
+}
+
+impl Io {
+    pub fn new() -> Io {
+        let stdout_handle = unsafe { GetStdHandle(STD_OUTPUT_HANDLE) };
+        let stderr_handle = unsafe { GetStdHandle(STD_ERROR_HANDLE) };
+        let input_handle = unsafe { GetStdHandle(STD_INPUT_HANDLE) };
+
+        Io { stdout_handle, stderr_handle, input_handle }
+    }
 }
