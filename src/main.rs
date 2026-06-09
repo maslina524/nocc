644d1f231bd190cacd21b21f6b950631ead5b51a
maslina524 <luk.kirill2804@gmail.com>
2026-06-10 02:54:47 +0300

    Added print current user name

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,12 @@ use os::get_user_name;
 
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    let io = Io::new();
+    io.print("panicked\n");
+    if let Some(message) = info.message().as_str() {
+        io.print(message);
+    }
     loop {}
 }
 
@@ -34,9 +39,21 @@ pub extern "C" fn main() -> i32 {
     let mut info_buf = [""; LINES];
 
     // USER NAME@PC NAME
-    let binding = get_user_name();
     let mut cur_buf = [0; 255];
-    let v = "\x1b[1;34m".as_bytes();
+    paste_to_buf(&mut cur_buf, "\x1b[1;34m".as_bytes(), 0);
+    let mut pos = "\x1b[1;34m".len();
+
+    let name = get_user_name();
+    let name_len = name.iter().position(|&b| b == 0).unwrap_or(name.len());
+    paste_to_buf(&mut cur_buf, &name[..name_len], pos);
+    pos += name_len;
+
+    let string = "\x1b[0m@\x1b[1;34m";
+    paste_to_buf(&mut cur_buf, string.as_bytes(), pos);
+    pos += string.len();
+
+    paste_to_buf(&mut cur_buf, "PC_NAME".as_bytes(), pos);
+    info_buf[0] = unsafe { str::from_utf8_unchecked(&cur_buf) };
 
     // PRINT BUFFERS
     for i in 0..LINES {
@@ -55,8 +72,7 @@ pub extern "C" fn main() -> i32 {
     0
 }
 
-fn paste_to_buf(buf: &mut [u8], string: &str, index: usize) {
-    let bytes = string.as_bytes();
+fn paste_to_buf(buf: &mut [u8], bytes: &[u8], index: usize) {
     for (i, ch) in bytes.iter().enumerate() {
         buf[index + i] = ch.clone();
     }
