3cd71cf1c2f75e03c9e2f7785ecf278794dd7f0b
maslina524 <luk.kirill2804@gmail.com>
2026-06-10 15:09:41 +0300

    Added print splitter & os name

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ mod os;
 use core::panic::PanicInfo;
 use io::Io;
 use logos::*;
+use windows::types::RTL_OSVERSIONINFOW;
+
+use crate::os::get_os_ver;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -51,6 +54,15 @@ pub extern "C" fn main() -> i32 {
     let info_line = core::str::from_utf8(&cur_buf[..pos]).unwrap_or("Invalid UTF-8");
     info_buf[0] = info_line;
 
+    // SPLITTER
+    info_buf[1] = "---------------";
+
+    // OS NAME
+    let osvi = get_os_ver();
+    let mut temp_buf = [0u8; 256];
+    let os_str = os_ver_as_str(&mut temp_buf, osvi);
+    info_buf[2] = os_str;
+
     // PRINT BUFFERS
     for i in 0..LINES {
         let string = logo_buf[i];
@@ -73,4 +85,37 @@ fn paste_to_buf(buf: &mut [u8], bytes: &[u8], index: usize) -> usize {
         buf[index + i] = *ch;
     }
     bytes.len()
+}
+
+fn u32_to_str<'a>(mut n: u32, buffer: &'a mut [u8; 10]) -> &'a str {
+    let mut idx = buffer.len();
+    
+    if n == 0 {
+        buffer[idx - 1] = b'0';
+        idx -= 1;
+    } else {
+        while n > 0 {
+            idx -= 1;
+            buffer[idx] = b'0' + (n % 10) as u8;
+            n /= 10;
+        }
+    }
+
+    let bytes = &buffer[idx..];
+    unsafe { core::str::from_utf8_unchecked(bytes) }
+}
+
+fn os_ver_as_str<'a>(temp_buf: &'a mut [u8], osvi: RTL_OSVERSIONINFOW) -> &'a str {
+    let name = if cfg!(target_os = "windows") {
+        "Windows"
+    } else if cfg!(target_os = "linux") {
+        "Linux"
+    } else if cfg!(target_os = "macos") {
+        "MacOS"
+    } else {
+        "Unknown"
+    };
+
+    let len = paste_to_buf(temp_buf, name.as_bytes(), 0);
+    unsafe { core::str::from_utf8_unchecked(&temp_buf[..len]) }
 }
