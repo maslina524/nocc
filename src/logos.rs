5e607b37c111158dc9b263889837d2d3cd5a9384
maslina524 <luk.kirill2804@gmail.com>
2026-06-11 23:33:50 +0300

    All logos moved to logos/ dir

--- src/logos.rs
+++ src/logos.rs
@@ -1,56 +1,6 @@
 // 37 x 15
 pub const LINES: usize = 15;
 
-pub const WIN11: [&'static str; 15] = [
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWW",
-];
-
-pub const WIN10_8: [&'static str; 15] = [
-    "\x1b[1;34m                        ...,,,;;;wwWW",
-    "\x1b[1;34m           ...,,   ;wwWWWWWWWWWWWWWWW",
-    "\x1b[1;34m.,,,;;;wwWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "                                               ",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34mWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34m`^^^+++\\\\WWWWWWW   WWWWWWWWWWWWWWWWWW",
-    "\x1b[1;34m           ```^^   +\\\\WWWWWWWWWWWWWWW",
-    "\x1b[1;34m                        ```^^^+++\\\\WW"
-];
-
-pub const WIN7: [&'static str; 15] = [
-    "\x1b[1;31m        wWWWWWWWWWw                  ",
-    "\x1b[1;31m       wWWWWWWWWWWWW    w--      --/w",
-    "\x1b[1;31m      WWWWWWWWWWWWWw   wWWWWWWWWWWWWW",
-    "\x1b[1;31m     wWWWWWWWWWWWWw   wWWWWWWWWWWWW' ",
-    "\x1b[1;31m    'WWWWWWWWWWWWw   wWWWWWWWWWWWWW  ",
-    "\x1b[1;31m    W/--      --w    '*WWWWWWWWWW*'  ",
-    "\x1b[1;34m                       '*WWWWWWW*'   ",
-    "\x1b[1;34m    wWWWWWWWWWw                      ",
-    "\x1b[1;34m   wWWWWWWWWWWWW    w--      --/w    ",
-    "\x1b[1;34m  wWWWWWWWWWWWWw   wWWWWWWWWWWWWW    ",
-    "\x1b[1;34m wWWWWWWWWWWWWw   WWWWWWWWWWWWW'     ",
-    "\x1b[1;34m'WWWWWWWWWWWWw   wWWWWWWWWWWWWW      ",
-    "\x1b[1;34mW/--      --w    '*WWWWWWWWWW*'      ",
-    "\x1b[1;34m                   '*WWWWWWW*'       ",
-    "\x1b[1;34m                                     ",
-];
+pub const WIN11: &str = include_str!("../logos/windows11.txt");
+pub const WIN10_8: &str = include_str!("../logos/windows10_8.txt");
+pub const WIN7: &str = include_str!("../logos/windows7.txt");
